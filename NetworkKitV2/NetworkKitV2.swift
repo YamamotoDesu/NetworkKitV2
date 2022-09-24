e2667eef9a0dfe169230f79e841623f1a16ecae1
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2022-09-24 14:10:46 +0900

    Initial Commit

---
+++ NetworkKitV2/NetworkKitV2.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkKitV2.swift
+//  NetworkKit2
+//
+//  Created by 山本響 on 2022/09/24.
+//
+
+import Foundation
+
+public class NetworkKitV2Imp {
+    
+    public init() {}
+    
+    public func getSth() {
+        
+    }
+}
